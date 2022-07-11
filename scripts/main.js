14e7d637c8a7b0a9e05b6005da5e650ead04b226
Freetzen <federuizgei@gmail.com>
2022-07-11 15:57:26 -0300

    Actualizando página

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+
+/* Multiplicación */
+
+/* let numero1 = 5;
+
+let numero2 = 10;
+
+let resultado = numero1 * numero2;
+
+alert ("El resultado es: " + resultado) */
+
+
+
+
+/* let dato = prompt("Ingrese año de Nacimiento:");
+
+const anio_actual = 2022;
+
+let edad = anio_actual - dato;
+
+alert(edad); */
